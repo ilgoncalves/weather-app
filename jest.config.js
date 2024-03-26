03c32397a74d3248ec1dac9361e1deaaabbbd02e
Igor Gonçalves <igor.lima-sevenapps@bemobi.com>
2024-03-25 22:32:38 -0300

    chore: improve boilerplate configs

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.[jt]sx?$': 'babel-jest',
+    '.+\\.(css|scss|png|jpg|svg)$': 'jest-transform-stub',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-leaflet|@react-leaflet|d3-*|axios))',
+  ],
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+    '^axios$': require.resolve('axios'),
+    '\\.scss$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
