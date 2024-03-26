fb2ec4bbe26f339846aa8202e910d859f7f18be3
Igor Gonçalves <igor.lima-sevenapps@bemobi.com>
2024-03-25 22:46:00 -0300

    chore: some config files

---
+++ src/application/config/env.ts
@@ -0,0 +1,7 @@
+class Config {
+  static readonly OPEN_WEATHER_MAP_API_URL =
+    'https://api.openweathermap.org/data/2.5';
+  static readonly OPEN_WEATHER_MAP_API_KEY = 'sua_chave_de_api_aqui';
+}
+
+export default Config;
