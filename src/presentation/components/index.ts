c361b3ee874596176ecaa6113276e2805a81af27
Igor Gonçalves <igor.lima-sevenapps@bemobi.com>
2024-03-25 22:48:28 -0300

    chore: export components

---
+++ src/presentation/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './header';
+export { default as Icon } from './icon';
+export { default as Select } from './select';
+export { default as Switch } from './switch';
